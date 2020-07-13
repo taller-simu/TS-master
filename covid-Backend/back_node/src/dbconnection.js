b2c84a702fe27732fb0921ee6455827517685ebc
arnol <arnol01233@gmail.com>
2020-07-13 16:59:56 -0400

    deleting docs

--- covid-Backend/back_node/src/dbconnection.js
+++ covid-Backend/back_node/src/dbconnection.js
@@ -19,32 +19,3 @@ mysqlConnection.connect((err)=>{
 
 module.exports = mysqlConnection;
 
-/*//Create database
-app.get('/createdb',(req, res)=>{
-    let sql = 'CREATE DATABASE basededatos';
-    con.query(sql, (err,result)=>{
-        if (err) throw err;
-        console.log(result);
-        res.send("database created"); 
-    });
-});
-
-//Create Table
-app.get('/createtable',(req,res)=>{
-    let sql='CREATE TABLE datos(id int AUTO_INCREMENT, title VARCHAR(255),boDy VARCHAR(255), PRIMARY KEY(id))';
-    con.query(sql,(err,result)=>{
-        if (err) throw err;
-        console.log(result);
-        res.send("table created"); 
-    });
-});
-
-//insert post 
-app.get('/addpost1',(req,res)=>{
-    let post = {title:'post one', body:'body post'};
-    let
-});
-app.listen('3000',()=>{
-    console.log("sadsadad");
-});
-*/
